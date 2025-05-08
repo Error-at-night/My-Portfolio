e2197068791efd0f91069f6ca63e39953edb5db8
Error-at-night <olayemisuccess553@gmail.com>
2025-05-08 02:21:04 +0100

    edited the about me section2

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -7,10 +7,11 @@ function AboutMe() {
       <div className="mx-auto max-w-[1200px] mt-6">
         <h1 className="text-[#FFFFFF] font-extrabold text-[1.9rem] sm:text-[2rem] md:text-[2.5rem]">About Me</h1>
         <p className="text-white text-[1.3rem] mt-2 md:tracking-wide leading-loose">My name is Success Olayemi, I'm Frontend 
-          Developer that is passionate about developing web applications that solves real problems. My Tech Stack NextJs, ReactJs, 
-          TypeScript, Redux, React-Query (TanStack), Axios, React-Hook-Form, Supabase, TailwindCSS, ShadcnUI, Bootstrap, and Git. 
-          I am always open to learning new frameworks and technologies to deliver the best results. Let us connect and 
-          build something amazing together.
+          Developer that is passionate about developing web applications that solves real problems. I have had the 
+          opportunity to teach and mentor aspiring frontend developers, helping them understand modern web technologies and adopt 
+          best practices. I enjoy simplifying complex concepts and guiding others to become better developers. I am always open 
+          to learning new frameworks and technologies to deliver the best results. Let us connect and build something amazing 
+          together.
         </p>
         <div className="mt-5 max-w-[180px]">
           <Link to="mailto: olayemisuccess7@gmail.com" className="flex items-center justify-between px-5 pt-2 pb-2 bg-[#298E77] 
