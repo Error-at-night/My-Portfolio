594744d32de86fe7535a51407a8120d17ac62254
Error-at-night <olayemisuccess553@gmail.com>
2025-05-08 02:22:57 +0100

    edited the about me section3

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -6,7 +6,7 @@ function AboutMe() {
     <main className="custom-gradient px-8 pt-3 pb-12" id="about-me">
       <div className="mx-auto max-w-[1200px] mt-6">
         <h1 className="text-[#FFFFFF] font-extrabold text-[1.9rem] sm:text-[2rem] md:text-[2.5rem]">About Me</h1>
-        <p className="text-white text-[1.3rem] mt-2 md:tracking-wide leading-loose">My name is Success Olayemi, I'm Frontend 
+        <p className="text-white text-[1rem] md:text-[1.3rem] mt-2 md:tracking-wide leading-normal md:leading-loose">My name is Success Olayemi, I'm Frontend 
           Developer that is passionate about developing web applications that solves real problems. I have had the 
           opportunity to teach and mentor aspiring frontend developers, helping them understand modern web technologies and adopt 
           best practices. I enjoy simplifying complex concepts and guiding others to become better developers. I am always open 
