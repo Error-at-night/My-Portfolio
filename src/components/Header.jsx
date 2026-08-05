1edd0b53edbf893d4d0273480357cc16da50c419
error-at-night <olayemisuccess553@gmail.com>
2026-08-04 23:00:23 -0700

    added my cv to the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import Navbar from "./Navbar"
 import banner from "../assets/banner.png"
-// import arrow from "../assets/arrow-up-right.png"
-// import resume from '../assets/SUCCESS-OLAYEMI-RESUME.pdf';
+import arrow from "../assets/arrow-up-right.png"
+import cv from '../assets/Success-Olayemi-CV.pdf'
 
 function Header() {
   return (
@@ -15,16 +15,16 @@ function Header() {
             Hey, I am a <span className="text-[#298E77]">Software Engineer</span>. 
             Building your dream <span className="text-[#298E77]">pixel</span> by <span className="text-[#298E77]">pixel</span>!
           </h1>
-          {/* <a className="flex justify-between items-center max-w-[154px] mx-auto mt-6 px-5 pt-3 pb-3 bg-[#298E77]
+          <a className="flex justify-between items-center max-w-[154px] mx-auto mt-6 px-5 pt-3 pb-3 bg-[#298E77]
             text-white font-extrabold rounded-full cursor-pointer shadow-[0_0_10px_#D9D9D9C9] 
             hover:shadow-[0_0_20px_#D9D9D9C9] transition-shadow duration-300"
-            href={resume}
+            href={cv}
             download
             target="_blank"
           >
             <span>My Resume</span>
             <img src={arrow} alt="arrow" className="mt-1 w-[18px] h-auto ms-1"/>
-          </a> */}
+          </a>
         </div>
       </div>
       <Navbar/>
